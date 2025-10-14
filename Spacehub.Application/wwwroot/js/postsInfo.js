83f0d5e2d64bcdcf712a805710820626d1871dc3
Agustin-Ibarra <agustingsoc@gmail.com>
2025-10-14 19:20:57 -0300

    desarrollar funcionalidad para consumir api /api/posts/info/suggestion

--- Spacehub.Application/wwwroot/js/postsInfo.js
+++ Spacehub.Application/wwwroot/js/postsInfo.js
@@ -1,8 +1,9 @@
+const $body = document.querySelector("body");
+
 fetch(`/api/posts/info/${sessionStorage.getItem("id")}`)
 .then(async(response)=>{
   if(response.status === 200){
     const post = await response.json();
-    console.log(post);
     const text = post.textContent.replace(/\r?\n/g, '<br/><br/>');
     const $title = document.querySelector(".object-info-title");
     const $category = document.querySelector(".category");
@@ -21,4 +22,42 @@ fetch(`/api/posts/info/${sessionStorage.getItem("id")}`)
 })
 .catch((error)=>{
   console.log(error);
+})
+
+fetch("/api/posts/info/suggestion")
+.then(async(response)=>{
+  if(response.status === 200){
+    const suggestions = await response.json();
+    const $list = document.querySelector(".object-sub-list");
+    suggestions.forEach(suggestion => {
+      const $item = document.createElement("li");
+      const $divImage = document.createElement("div");
+      const $image = document.createElement("img");
+      const $title = document.createElement("p");
+      const $link = document.createElement("a");
+      $list.appendChild($item);
+      $item.setAttribute("class","object-sub-list-item");
+      $item.appendChild($divImage);
+      $item.appendChild($title);
+      $item.appendChild($link);
+      $divImage.setAttribute("class","object-sub-list-img-div");
+      $divImage.appendChild($image);
+      $image.setAttribute("src",suggestion.imagePath);
+      $title.setAttribute("class","object-sub-list-title");
+      $title.textContent = suggestion.title
+      $link.setAttribute("class","object-sub-list-link");
+      $link.setAttribute("href","/posts/info")
+      $link.setAttribute("id",suggestion.id);
+      $link.textContent = "Ver mas";
+    });
+  }
+})
+.catch((error)=>{
+  console.log(error);
+})
+
+$body.addEventListener("click",(e)=>{
+  if(e.target.matches(".object-sub-list-link")){
+    sessionStorage.setItem("id",e.target.id);
+  }
 })
